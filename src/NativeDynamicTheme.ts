7d6b52c2dd2de6ce966a2af65b182758e39dd199
FouadMagdy01 <fouad.magdy772@gmail.com>
2025-03-10 18:12:15 +0300

    revert: :rewind: make types in the same Spec file in order to make codegen wotks

--- src/NativeDynamicTheme.ts
+++ src/NativeDynamicTheme.ts
@@ -1,6 +1,49 @@
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
-import type { DynamicColorScheme } from './types/themeTypes';
+
+export type ColorScheme = {
+  primary: string;
+  onPrimary: string;
+  primaryContainer: string;
+  onPrimaryContainer: string;
+  inversePrimary: string;
+  secondary: string;
+  onSecondary: string;
+  secondaryContainer: string;
+  onSecondaryContainer: string;
+  tertiary: string;
+  onTertiary: string;
+  tertiaryContainer: string;
+  onTertiaryContainer: string;
+  background: string;
+  onBackground: string;
+  surface: string;
+  onSurface: string;
+  surfaceVariant: string;
+  onSurfaceVariant: string;
+  surfaceTint: string;
+  inverseSurface: string;
+  inverseOnSurface: string;
+  error: string;
+  onError: string;
+  errorContainer: string;
+  onErrorContainer: string;
+  outline: string;
+  outlineVariant: string;
+  scrim: string;
+  surfaceBright: string;
+  surfaceDim: string;
+  surfaceContainer: string;
+  surfaceContainerHigh: string;
+  surfaceContainerHighest: string;
+  surfaceContainerLow: string;
+  surfaceContainerLowest: string;
+};
+
+export type DynamicColorScheme = {
+  light: ColorScheme;
+  dark: ColorScheme;
+};
 
 export interface Spec extends TurboModule {
   getDynamicColorScheme(): DynamicColorScheme;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import DynamicTheme from './NativeDynamicTheme';
-import type { DynamicColorScheme } from './types/themeTypes';
-export { type DynamicColorScheme, type ColorScheme } from './types/themeTypes';
+import DynamicTheme, { type DynamicColorScheme } from './NativeDynamicTheme';
+
 import { Platform } from 'react-native';
 export function getDynamicColorScheme(): DynamicColorScheme | void {
   if (Platform.OS == 'android' && Platform.Version >= 31) {
     return DynamicTheme.getDynamicColorScheme();
   }
 }
+export {
+  type DynamicColorScheme,
+  type ColorScheme,
+} from './NativeDynamicTheme';
